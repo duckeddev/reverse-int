deeb855a0919caf43724c0c65d0a2623793782d9
vpikta <vpikta94@gmail.com>
2020-02-25 00:52:20 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    return Math.sign(n) != -1 ? +n.toString().split('').reverse().join('') : +Math.abs(n).toString().split('').reverse().join('');
 }
